39e1e210bfb508d965584e08cb229f9fd48799be
mostafa <mostafamnbvcxz5@gmail.com>
2021-12-19 23:36:27 +0330

    make main container part

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,35 @@
 const lis = document.querySelectorAll('header li a');
+const typeText = document.querySelector('.type');
+const Texts = [`
+function person() {
+    this.name : ‘mostafa kheibary’;
+    this.live : {
+        Country : ‘Iran’;
+        City : ‘Tehran’; 
+    } 
+}
+`];
 
-for(const li of lis){
-    li.addEventListener("click",()=>{
-        for(const li of lis){
+
+// menu clicked efect
+for (const li of lis) {
+    li.addEventListener("click", () => {
+        for (const li of lis) {
             li.classList.remove('active');
         }
         li.classList.add('active');
     });
 }
+
+// type writher effect
+let position = 0;
+function write() {
+    typeText.innerHTML = Texts[0].substring(0,position);
+    if (position++ != Texts[0].length) {
+        setTimeout(write, 100);
+    }
+}
+
+window.addEventListener('load',()=>{
+    write();
+})
