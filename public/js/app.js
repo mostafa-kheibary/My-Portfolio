cdffe2fdaa756875af211689a96193551852fe32
mostafa <mostafamnbvcxz5@gmail.com>
2022-01-08 20:04:30 +0330

    add working contact sections and the scroll efect

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,24 @@
-const Lis = document.querySelectorAll('header li');
-const Inicator = document.querySelector('.line-spect')
-const typeText = document.querySelector('.type');
+const $ = document;
+const Lis = $.querySelectorAll('header li');
+const header = $.querySelector('header');
+const LisLink = $.querySelectorAll('header li a');
+const Inicator = $.querySelector('.line-spect')
+const typeText = $.querySelector('.type');
+const loader  = $.querySelector('.loader');
+const Elavator = $.querySelector('.elavator');
+const Submit = $.querySelector('.submit-form')
+const NameInput = $.querySelector('.name-input');
+const EmailInput = $.querySelector('.email-input');
+const EmailError = $.querySelector('.lable-email');
+const FormError =  $.querySelector('.lable-form');
+const MessageInput = $.querySelector('.message-input');
+// **
+const intro = $.querySelector('.intro');
+const pro = $.querySelector('.pro');
+const cvlink = $.querySelector('.cvlink');
+const contact = $.querySelector('.contact');
+// **
+const Sections = $.querySelectorAll('.sec');
 const Texts = [`
 function person() {
     this.name : ‘mostafa kheibary’;
@@ -10,6 +28,67 @@ function person() {
     } 
 }
 `];
+
+//My functions :
+function SendContact(){
+    console.log(EmailInput.value)
+   const templateParams = {
+       from_name: NameInput.value,
+       email: EmailInput.value,
+       message: MessageInput.value
+   };
+   emailjs.send('service_t20nf7t', 'template_9juwq3e', templateParams)
+       .then(function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+       }, function(error) {
+          console.log('FAILED...', error);
+       });
+}
+function Scroll(){
+    if(scrollY >= 200){
+        Elavator.classList.add('elavator-show')
+    }
+    else{
+        Elavator.classList.remove('elavator-show')
+    }
+    if(scrollY >= 150){
+        header.classList.add('header-show');
+    }
+    else{
+        header.classList.remove('header-show');
+    }
+    console.log(scrollY);
+    if(scrollY<= 200){
+        intro.classList.add('yellow');
+    }
+    else{
+        intro.classList.remove('yellow');
+    }
+    if(scrollY>200&& scrollY<1012){
+        pro.classList.add('yellow');
+    }
+    else{
+        pro.classList.remove('yellow');
+    }
+    if(scrollY>1012&& scrollY<1570){
+        cvlink.classList.add('yellow');
+    }
+    else{
+        cvlink.classList.remove('yellow');
+    }
+    if(scrollY>1570){
+        contact.classList.add('yellow');
+    }
+    else{
+        contact.classList.remove('yellow');
+    }
+}
+// My event :
+
+
+
+// hide scroll bar 
+document.body.style.overflow = 'hidden';
 // type writher effect
 let position = 0;
 function write() {
@@ -19,13 +98,16 @@ function write() {
     }
 }
 
-window.addEventListener('load',()=>{
-    write();
-})
-
-
-
+// li hover efect
 for(const li of Lis){
+    li.addEventListener('click',()=>{
+        for(const sec of Sections){
+           if(li.innerText === sec.dataset.num){
+                window.scrollBy(0,sec.offsetTop-200);
+           }
+        }
+        
+    })
     li.addEventListener('mouseover',()=>{
         Inicator.style.transform = `scale(1)`
         Inicator.style.left = `${li.offsetLeft}px`
@@ -35,3 +117,40 @@ for(const li of Lis){
         Inicator.style.transform = `scale(0)`
     })
 }
+// loader 
+window.addEventListener('load',()=>{
+    setTimeout(()=>{
+        write();
+        loader.classList.add('loader-hide');
+        document.body.style.overflow = 'visible';
+    },500)
+});
+
+window.addEventListener('scroll',()=>{
+   Scroll();
+})
+
+EmailInput.addEventListener('keyup',()=>{
+    const value = EmailInput.value;
+    FormError.classList.remove('lable-show');
+    if(value.includes('.')&&value.includes('@')){
+        EmailError.classList.remove('.lable-show');
+    }
+    else{
+        EmailError.classList.add('.lable-show');
+    }
+    
+})
+
+Submit.addEventListener('click',()=>{
+    const value = EmailInput.value;
+    if(value.includes('.')&&value.includes('@')){
+        SendContact();
+        NameInput.value = '';
+        EmailInput.value = '';
+        MessageInput.value = '';
+    }
+    else{
+        FormError.classList.add('lable-show');
+    }
+})
